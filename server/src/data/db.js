2a00fde49535d3b5b6d6ade0603375f938924c7d
Nenad Brcic <neno@neno.ch>
2016-02-24 18:02:32 +0100

    Hard coding mongolab connection string

--- server/src/data/db.js
+++ server/src/data/db.js
@@ -1,18 +1,19 @@
 // ### Database connection module using mongojs
 var mongojs          = require('mongojs')
-  , connectionString = process.env.MONGO_CONN_STRING     // mandatory env var
-  , collections;
+//  , connectionString = process.env.MONGO_CONN_STRING     // mandatory env var
+, connectionString = 'mongodb://heroku_w5067d4b:ck74amh8i46vrdk9h37f5fu2pv@ds055915.mongolab.com:55915/heroku_w5067d4b'     // mandatory env var
+, collections;
 
 // Enumerate all MongoDB collections to expose
 collections = [
     'locations'
-  , 'deadlines'
-  , 'news'
-  , 'civilRights'
-  , 'labels'
+    , 'deadlines'
+    , 'news'
+    , 'civilRights'
+    , 'labels'
 ];
 
 module.exports = {
     db        : mongojs.connect(connectionString, collections)
-  , ObjectId  : mongojs.ObjectId    // a function that generates mongo object ID
+    , ObjectId  : mongojs.ObjectId    // a function that generates mongo object ID
 };
